cf9caae63c023ad87504c4ea13f0c1c92290bb40
Gabrielle Coelho Silveira <gabrielle.263817@alunosatc.edu.br>
2024-09-11 20:55:09 -0300

    atualizacao

--- exercicio07/src/main/java/com/example/demo/MovieRecomendationController.java
+++ exercicio07/src/main/java/com/example/demo/MovieRecomendationController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@GetMapping("/exercicio")
 public class MovieRecomendationController {
     @GetMapping("/recomendar")
     public String recomendarFilme(
